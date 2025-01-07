5eb6069289687107a930da455f9e96db9575af9a
Abeeha-Z <158211569+Abeeha-Z@users.noreply.github.com>
2025-01-07 14:00:08 -0500

    gydf

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,45 +1,43 @@
-import "./CSS/style.css"
-const task_btn = document.getElementById("task_btn")
+import "/CSS/style.css"
+const task_btn = document.getElementById("task_btn");
 const container = document.getElementById("task_container");
-const menu = document.getElementById("menu")
+const menu = document.getElementById("menu");
 
-task_btn.addEventListener("event", function() {
+task_btn.addEventListener("click", function() {
     menu.innerHTML = 
     `
-    <form id="taskform" type="submit">
-        <input id="name">
-    <label for="name">Task: </label> <br>
-        <input id="ddl">
-    <label for="ddl">Deadline: </label> <br>
-        <input id="priority">
-    <label for="priority">Priority(high/med/low): </label> <br>
+    <form>
+    <label for="name">Task: </label>
+    <input id="name"><br>
+    <label for="ddl">Deadline: </label>
+    <input id="ddl"><br>
+    <label for="priority">Priority(high/med/low): </label>
+    <input id="priority"><br>
+    <button type="submit" id="submit_btn">Submit</button>
     </form>    
     `
-    });
+    const submit_btn = document.getElementById("submit_btn")
 
-    task_form = document.getElementById("taskform")
-
-    task_form.addEventListener("submit", function() {
+    submit_btn.addEventListener("submit", function() {
         addTask();
     });
 
-
+    });
 
 function addTask() {
-    task_name = document.getElementById("name").value
-    deadline = document.getElementById("ddl").value
-    priority = document.getElementById("priority").value.lower();
+    const task_name = document.getElementById("name").value
+    const deadline = document.getElementById("ddl").value
+    const priority = document.getElementById("priority").value
 
     container.insertAdjacentHTML("beforeend", `
     <div>
-    <input type="checkbox" id="task" checked />
-    <label for="task">${task_name}, due ${deadline}, priority: ${priority}</label>
+    <p>${task_name}, due ${deadline}, priority: ${priority}</p>
   </div>
         `)
-
 }
 
-
+//    <input type="checkbox" id="task" checked />
+//<label for="task">${task_name}, due ${deadline}, priority: ${priority}</label>
 // eventlis for taskbtn --- show form -- event listener for form submission --- run addTask()
 
 
